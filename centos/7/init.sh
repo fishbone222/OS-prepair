eab360aaa0bc58dec3cee49b32482d2876fcd25b
fishbone222 <github@fischer-siegen.de>
2021-06-15 21:58:38 +0200

    Update init.sh

--- centos/7/init.sh
+++ centos/7/init.sh
@@ -1,5 +1,6 @@
 yum install -y wget epel-release
 yum install -y nmap htop vim openssh-server dnf dnf-automatic
+yum upgrade -y
 systemctl enable --now sshd
 echo "export TERM=xterm" >> /etc/bashrc
 useradd -m fishbone222
