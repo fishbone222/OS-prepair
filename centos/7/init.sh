717636e33d9c117b82691a0f03d8b4d379b7f46f
Till Fischer <github@fischer-siegen.de>
2021-06-15 21:54:27 +0200

    geändert:       centos/7/init.sh

--- centos/7/init.sh
+++ centos/7/init.sh
@@ -1,5 +1,6 @@
 yum install -y wget epel-release
-yum install -y nmap htop vim
+yum install -y nmap htop vim openssh-server
+systemctl enable --now sshd
 echo "export TERM=xterm" >> /etc/bashrc
 useradd -m fishbone222
 passwd fishbone222
