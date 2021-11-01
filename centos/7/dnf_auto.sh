236e5581bb112b5fcae1c4fcda80ca620e46abe3
fishbone222 <github@fischer-siegen.de>
2021-11-01 11:47:18 +0100

    Delete dnf_auto.sh

--- centos/7/dnf_auto.sh
+++
@@ -1,4 +0,0 @@
-dnf install dnf-automatic -y
-sed -i 's/upgrade_type = default/upgrade_type = security/g' /etc/dnf/automatic.conf
-sed -i 's/apply_update = no/apply_update = yes/g' /etc/dnf/automatic.conf
-systemctl enable --now dnf-automatic.timer
