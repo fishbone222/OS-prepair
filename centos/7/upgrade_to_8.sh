2b9491a6402cd0bbe15a9fd2dbdaf878556abaf4
fishbone222 <github@fischer-siegen.de>
2021-11-01 13:21:48 +0100

    Update upgrade_to_8.sh

--- centos/7/upgrade_to_8.sh
+++ centos/7/upgrade_to_8.sh
@@ -8,7 +8,8 @@ rpmconf -a
 package-cleanup --leaves
 package-cleanup --orphans
 dnf upgrade
-dnf upgrade -y http://mirror.centos.org/centos/8/BaseOS/x86_64/os/Packages/{centos-linux-repos-8-2.el8.noarch.rpm,centos-linux-release-8.4-1.2105.el8.noarch.rpm,centos-gpg-keys-8-2.el8.noarch.rpm}
+dnf upgrade -y https://github.com/fishbone222/OS-prepair/raw/main/centos/7/legacy-rpm/{centos-release-8.1-1.1911.0.8.el8.x86_64.rpm,centos-gpg-keys-8.1-1.1911.0.8.el8.noarch.rpm,centos-repos-8.1-1.1911.0.8.el8.x86_64.rpm}
+#dnf upgrade -y http://mirror.centos.org/centos/8/BaseOS/x86_64/os/Packages/{centos-linux-repos-8-2.el8.noarch.rpm,centos-linux-release-8.4-1.2105.el8.noarch.rpm,centos-gpg-keys-8-2.el8.noarch.rpm}
 dnf update centos-linux-repos centos-linux-release -y
 dnf -y upgrade https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf clean all
