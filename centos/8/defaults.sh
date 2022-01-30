c647ad2234b540b5e4abc7c763cc7b5ddee31ee7
fishbone222 <github@fischer-siegen.de>
2022-01-30 12:59:11 +0100

    Update defaults.sh

--- centos/8/defaults.sh
+++ centos/8/defaults.sh
@@ -1,4 +1,5 @@
-dnf install -y vim htop nmap wget
+dnf install -y epel-release
+dnf install -y vim htop nmap wget openssh-server
 timedatectl set-timezone Europe/Berlin
 curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/centos/8/fix-lxc-filesystem.sh | sh
 dnf upgrade -y
