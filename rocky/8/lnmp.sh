6d13da66d53a5f62ad52c31dcccc83bf3f45b9a1
fishbone222 <github@fischer-siegen.de>
2022-01-31 22:45:25 +0100

    Update and rename nginx.sh to lnmp.sh

---
+++ rocky/8/lnmp.sh
@@ -0,0 +1,15 @@
+dnf install nginx mariadb-server mod_ssl -y
+dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module reset php -y
+dnf module enable php:remi-8.0 -y
+dnf install php php-fpm php-gd php-mysqlnd php-mbstring -y
+
+sed -i 's/^user.*=*$/user = nginx/' /etc/php-fpm.d/www.conf
+sed -i 's/^group.*=*$/group = nginx/' /etc/php-fpm.d/www.conf
+chown -R root:nginx /var/lib/php
+
+systemctl enable --now php-fpm
+systemctl enable --now nginx
+systemctl enable --now mariadb
+chcon -Rt httpd_sys_rw_content_t /var/www
+read -p "reboot and run $(pwd)/lnmp-2.sh"
