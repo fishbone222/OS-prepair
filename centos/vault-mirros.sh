6a51ca7f74d5d4cbf6297b23f2f3070e5e93dd42
fishbone222 <github@fischer-siegen.de>
2022-04-05 12:13:56 +0200

    Update vault-mirros.sh

--- centos/vault-mirros.sh
+++ centos/vault-mirros.sh
@@ -1,3 +1,5 @@
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+dnf update -y
+dnf upgrade -y
+echo "Please reboot"
