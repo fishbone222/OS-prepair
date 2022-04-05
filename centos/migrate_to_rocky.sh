e415f11e40ce49d5d9ae5afc7182c656bb7b8851
fishbone222 <github@fischer-siegen.de>
2022-04-05 12:17:52 +0200

    Update migrate_to_rocky.sh

--- centos/migrate_to_rocky.sh
+++ centos/migrate_to_rocky.sh
@@ -1,9 +1,11 @@
 curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/centos/vault-mirros.sh | bash
 dnf install git -y
+dnf clean all
 cd /tmp/
 git clone https://github.com/rocky-linux/rocky-tools.git
 cd /tmp/rocky-tools/migrate2rocky
 chmod -v +x migrate2rocky.sh
 ./migrate2rocky.sh -r
+dnf clean all
 echo "Check /var/log/migrate2rocky.log"
 echo "Please reboot"
