ecec087df2ee6cab8621472f622d8aa5d184ea5a
fishbone222 <github@fischer-siegen.de>
2022-04-21 14:51:04 +0200

    Update defaults.sh

--- rocky/8/defaults.sh
+++ rocky/8/defaults.sh
@@ -1,5 +1,5 @@
 dnf install -y epel-release
-dnf install -y vim htop nmap wget openssh-server
+dnf install -y vim htop nmap wget openssh-server tar unzip
 timedatectl set-timezone Europe/Berlin
 #curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/centos/8/fix-lxc-filesystem.sh | sh
 dnf upgrade -y
