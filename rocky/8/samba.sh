0a527ab9f06a37cc07bc2e8bdb00c05926587f63
fishbone222 <github@fischer-siegen.de>
2022-04-21 15:28:57 +0200

    Update samba.sh

--- rocky/8/samba.sh
+++ rocky/8/samba.sh
@@ -5,7 +5,7 @@ systemctl enable --now sshd
 #PowerTools
 dnf install dnf-plugins-core -y
 dnf config-manager --set-enabled powertools
-dnf -y install docbook-style-xsl gcc gdb gnutls-devel gpgme-devel jansson-devel keyutils-libs-devel krb5-workstation libacl-devel libaio-devel libarchive-devel libattr-devel libblkid-devel libtasn1 libtasn1-tools libxml2-devel libxslt lmdb-devel openldap-devel pam-devel perl perl-ExtUtils-MakeMaker perl-Parse-Yapp popt-devel python3-cryptography python3-dns python3-gpg python36-devel readline-devel rpcgen systemd-devel tar zlib-devel flex bison dbus-devel python3-markdown
+dnf -y install docbook-style-xsl gcc gdb gnutls-devel gpgme-devel jansson-devel keyutils-libs-devel krb5-workstation libacl-devel libaio-devel libarchive-devel libattr-devel libblkid-devel libtasn1 libtasn1-tools libxml2-devel libxslt lmdb-devel openldap-devel pam-devel perl perl-ExtUtils-MakeMaker perl-Parse-Yapp popt-devel python3-cryptography python3-dns python3-gpg python36-devel readline-devel rpcgen systemd-devel tar zlib-devel flex bison dbus-devel python3-markdown perl-JSON
 wget https://download.samba.org/pub/samba/stable/samba-4.16.0.tar.gz
 tar xaf samba*
 cd samba*
