bf21dd1c2392041e8e6f0ea52777969793aec7d6
fishbone222 <github@fischer-siegen.de>
2022-04-21 20:27:14 +0200

    Update lam.sh

--- rocky/8/lam.sh
+++ rocky/8/lam.sh
@@ -1,1 +1,6 @@
-
+dnf module enable php:7.4 -y
+dnf install httpd httpd-tools php php-fpm php-mysqlnd php-opcache php-gd php-xml php-mbstring php-json php-gmp php-zip php-ldap -y
+sudo systemctl enable --now php-fpm httpd
+wget http://prdownloads.sourceforge.net/lam/ldap-account-manager-7.9-1.fedora.1.noarch.rpm
+dnf localinstall ldap-account-manager-*.rpm
+echo "http://(server IP or hostname)/lam"
