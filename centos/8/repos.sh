82b70ca1822e27dc700838bae7e6d624a626079e
fishbone222 <github@fischer-siegen.de>
2022-05-01 23:30:48 +0200

    Update repos.sh

--- centos/8/repos.sh
+++ centos/8/repos.sh
@@ -1,5 +1,5 @@
 #PowerTools
-yum install dnf-plugins-core -y
-yum config-manager --set-enabled PowerTools
+dnf install dnf-plugins-core -y
+dnf config-manager --set-enabled powertools
 #RPM Fusion
 dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
