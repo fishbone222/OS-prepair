ef0184fe06e0bd945d0e464c18f065b8b8ab6a09
fishbone222 <github@fischer-siegen.de>
2022-05-01 12:50:31 +0200

    Update ldap.sh

--- rocky/8/ldap.sh
+++ rocky/8/ldap.sh
@@ -2,4 +2,4 @@ dnf --enablerepo=plus -y install openldap-servers openldap-clients
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 chown ldap. /var/lib/ldap/DB_CONFIG
 systemctl enable --now slapd
-lappasswd
+slappasswd
