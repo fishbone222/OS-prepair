0c5b7bc174d268769732e6486b1f8f0cbb821412
fishbone222 <github@fischer-siegen.de>
2022-05-01 12:49:35 +0200

    Create ldap.sh

---
+++ rocky/8/ldap.sh
@@ -0,0 +1,5 @@
+dnf --enablerepo=plus -y install openldap-servers openldap-clients
+cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
+chown ldap. /var/lib/ldap/DB_CONFIG
+systemctl enable --now slapd
+lappasswd
