917d8a1c1fd76f56acf863dc2676761eeaff9e55
fishbone222 <github@fischer-siegen.de>
2022-05-09 22:32:57 +0200

    Create omada.sh

---
+++ rocky/8/omada.sh
@@ -0,0 +1,22 @@
+echo "curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/centos/8/omada.sh | sh -"
+https://raw.githubusercontent.com/fishbone222/OS-prepair/main/rocky/8/defaults.sh | bash
+echo '[mongodb-org]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.6/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc' > /etc/yum.repos.d/mongodb-org.repo
+dnf install mongodb-org-server -y
+
+rpm -i http://repo.iotti.biz/CentOS/8/noarch/lux-release-8-1.noarch.rpm
+dnf install -y apache-commons-daemon-jsvc
+
+echo "%_netsharedpath /sys:/proc" >> /etc/rpm/macros.dist
+dnf install -y nmap openssh-server vim java-1.8.0-openjdk wget tar
+systemctl enable --now sshd
+dnf upgrade -y
+
+cd /tmp/
+curl https://static.tp-link.com/upload/software/2022/202203/20220322/Omada_SDN_Controller_v5.1.7_Linux_x64.tar.gz | tar xvz
+reset
+./install.sh
