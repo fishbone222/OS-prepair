2ba83305f960bb3ef0fa53fb06bf6c8fc21a24b0
fishbone222 <github@fischer-siegen.de>
2022-05-16 18:42:49 +0200

    Update imap_move.sh

--- imap_move.sh
+++ imap_move.sh
@@ -1,6 +1,7 @@
+CSV=$(realpath $1)
 cd /tmp
 git clone https://github.com/imapsync/imapsync
 cd imapsync
 while IFS=, read -r host1 user1 password1 server2 user2 password2; do
    imapsync --host1 $host1 --user1 $user1 --password1 $password1 --host2 $host2 --user2 $user2 --password2 $password2
-done < $1
+done < $CSV
