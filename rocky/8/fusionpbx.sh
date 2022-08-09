a21147e709b56146eedcf5a066b4c5d3b7f0d114
fishbone222 <github@fischer-siegen.de>
2022-08-09 10:01:38 +0200

    Update fusionpbx.sh

--- rocky/8/fusionpbx.sh
+++ rocky/8/fusionpbx.sh
@@ -11,4 +11,16 @@ echo ""
 
 bash <(curl -s https://raw.githubusercontent.com/fishbone222/OS-prepair/main/rocky/8/defaults.sh)
 bash <(curl -s https://raw.githubusercontent.com/fishbone222/OS-prepair/main/rocky/8/dnf_auto.sh)
-bash <(curl -s https://github.com/fishbone222/fusionpbx-install.sh/blob/master/centos/pre-install.sh)
+bash <(curl -s https://raw.githubusercontent.com/fishbone222/fusionpbx-install.sh/master/centos/pre-install.sh)
+
+if [ "$sql" == "pgsql" ]; then
+  dnf module enable postgresql:13 -y
+  dnf install postgresql-server php-pgsql -y
+  postgresql-setup --initdb
+  sed -i s/ident/md5/ /var/lib/pgsql/data/pg_hba.conf
+  systemctl enable --now postgresql
+fi
+
+if [ "$sql" == "mysql" ]; then
+  dnf install mariadb -y
+fi
