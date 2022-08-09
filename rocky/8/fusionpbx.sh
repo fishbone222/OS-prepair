380df5f3babe449b6a273eff4b0f9dd0128ad732
fishbone222 <github@fischer-siegen.de>
2022-08-09 09:51:48 +0200

    Update fusionpbx.sh

--- rocky/8/fusionpbx.sh
+++ rocky/8/fusionpbx.sh
@@ -11,24 +11,4 @@ echo ""
 
 bash <(curl -s https://raw.githubusercontent.com/fishbone222/OS-prepair/main/rocky/8/defaults.sh)
 bash <(curl -s https://raw.githubusercontent.com/fishbone222/OS-prepair/main/rocky/8/dnf_auto.sh)
-sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
-dnf install epel-release -y
-rpm -ivh http://repo.okay.com.mx/centos/8/x86_64/release/okay-release-1-5.el8.noarch.rpm?
-if [ "$sql" == "pgsql" ]; then
-  dnf module enable postgresql:13 -y
-  dnf install postgresql-server php-pgsql -y
-  postgresql-setup --initdb
-  sed -i s/ident/md5/ /var/lib/pgsql/data/pg_hba.conf
-  systemctl enable --now postgresql
-fi
-systemctl stop firewalld
-dnf -y  install nginx fusionpbx-all fusionpbx-fail2ban-rules freeswitch-fail2ban-rules fail2ban-server fail2ban-systemd
-cat /usr/share/doc/fusionpbx-fail2ban-rules/README.fusionpbx-fail2ban-rules.txt /usr/share/doc/freeswitch-fail2ban-rules/README.freeswitch-fail2ban-rules.txt > /etc/fail2ban/jail.local
-systemctl enable --now nginx
-systemctl enable --now php-fpm
-systemctl enable --now freeswitch
-systemctl enable --now memcached
-if [ "$sql" == "mysql" ]; then
-  systemctl enable --now mariadb
-fi
-systemctl enable --now fail2ban
+bash <(curl -s https://github.com/fishbone222/fusionpbx-install.sh/blob/master/centos/pre-install.sh)
