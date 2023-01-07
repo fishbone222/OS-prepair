c6f0eabd458d114b2b7a5b669906ee735c2260e3
fishbone222 <github@fischer-siegen.de>
2023-01-07 12:10:44 +0100

    Update dnf_auto.sh

--- rocky/8/dnf_auto.sh
+++ rocky/8/dnf_auto.sh
@@ -1,3 +1,4 @@
+#curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/rocky/8/dnf_auto.sh | bash
 dnf install dnf-automatic -y
 sed -i 's/upgrade_type = default/upgrade_type = security/g' /etc/dnf/automatic.conf
 sed -i 's/apply_update = no/apply_update = yes/g' /etc/dnf/automatic.conf
