068a45005e7dd17cc0a3a32cd5b00d31bb152d23
fishbone222 <github@fischer-siegen.de>
2023-04-16 11:19:21 +0200

    Update defaults.sh

--- el/9/defaults.sh
+++ el/9/defaults.sh
@@ -1,3 +1,4 @@
+#curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/defaults.sh | bash
 dnf install -y epel-release
 dnf install -y vim htop nmap wget openssh-server tar unzip
 timedatectl set-timezone Europe/Berlin
