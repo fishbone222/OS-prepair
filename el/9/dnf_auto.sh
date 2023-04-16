8175a3018181db6493dd1a790502de6de5db8f8c
fishbone222 <github@fischer-siegen.de>
2023-04-16 11:06:13 +0200

    Create dnf_auto.sh

---
+++ el/9/dnf_auto.sh
@@ -0,0 +1,5 @@
+#curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/dnf_auto.sh | bash
+dnf install dnf-automatic -y
+sed -i 's/upgrade_type = default/upgrade_type = security/g' /etc/dnf/automatic.conf
+sed -i 's/apply_update = no/apply_update = yes/g' /etc/dnf/automatic.conf
+systemctl enable --now dnf-automatic.timer
