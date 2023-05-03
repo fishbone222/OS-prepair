9d9f19023e4c02c3abf992a45bb3310d4f745207
fishbone222 <github@fischer-siegen.de>
2023-05-03 10:03:30 +0200

    Update omada.sh

--- el/9/omada.sh
+++ el/9/omada.sh
@@ -8,8 +8,8 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc' > /etc/yum.repos.d/mongodb-org.repo
 dnf install mongodb-org-server -y
 
-rpm -i http://repo.iotti.biz/CentOS/8/noarch/lux-release-8-1.noarch.rpm
-dnf install -y apache-commons-daemon-jsvc
+#rpm -i http://repo.iotti.biz/CentOS/8/noarch/lux-release-8-1.noarch.rpm
+#dnf install -y apache-commons-daemon-jsvc
 
 echo "%_netsharedpath /sys:/proc" >> /etc/rpm/macros.dist
 dnf install -y nmap openssh-server vim java-1.8.0-openjdk wget tar
@@ -20,6 +20,6 @@ dnf autoremove -y
 dnf clean all
 
 cd /tmp/
-curl https://static.tp-link.com/upload/software/2022/202203/20220322/Omada_SDN_Controller_v5.1.7_Linux_x64.tar.gz | tar xvz
+curl https://static.tp-link.com/upload/software/2023/202303/20230321/Omada_SDN_Controller_v5.9.31_Linux_x64.tar.gz | tar xvz
 cd Omada*
 ./install.sh
