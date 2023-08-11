5d1cf09db747919c589f91f42d28d9a3ec3e05ec
fishbone222 <github@fischer-siegen.de>
2023-08-11 16:14:46 +0200

    Update fusionpbx.sh
    Install pgsql14

--- el/8/fusionpbx.sh
+++ el/8/fusionpbx.sh
@@ -6,4 +6,10 @@ bash <(curl -s https://raw.githubusercontent.com/fishbone222/fusionpbx-install.s
 
 adduser freeswitch -G daemon
 
+dnf -qy module disable postgresql
+dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+dnf install -y postgresql14-server
+ln -s /usr/bin/postgresql-setup /usr/pgsql-14/bin/postgresql-14-setup
+ln -s /etc/systemd/system/postgresql-14.service ln -s /etc/systemd/system/postgresql.service
+
 cd /usr/src/fusionpbx-install.sh/centos && ./install.sh
