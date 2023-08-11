c35316164ac048574815bb341f11c7334d17939e
fishbone222 <github@fischer-siegen.de>
2023-08-11 12:59:03 +0200

    Update fusionpbx.sh
    add freeswitch user

--- el/8/fusionpbx.sh
+++ el/8/fusionpbx.sh
@@ -4,4 +4,6 @@ bash <(curl -s https://raw.githubusercontent.com/fishbone222/OS-prepair/main/roc
 bash <(curl -s https://raw.githubusercontent.com/fishbone222/OS-prepair/main/rocky/8/dnf_auto.sh)
 bash <(curl -s https://raw.githubusercontent.com/fishbone222/fusionpbx-install.sh/master/centos/pre-install.sh)
 
+adduser freeswitch -G daemon
+
 cd /usr/src/fusionpbx-install.sh/centos && ./install.sh
