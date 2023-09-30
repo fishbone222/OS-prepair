f0adac19acf9c633c4f4dbc49ea3252b074a2cd8
fishbone222 <github@fischer-siegen.de>
2023-09-30 18:17:19 +0200

    Update zoneminder.sh

--- el/9/zoneminder.sh
+++ el/9/zoneminder.sh
@@ -1,7 +1,7 @@
 #curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/zoneminder.sh | bash
 curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/defaults.sh | bash
-dnf install dnf-plugins-core
+dnf install dnf-plugins-core -y
 dnf config-manager --set-enabled crb
-dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
-dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
-dnf install zoneminder-nginx
+dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm -y
+dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
+dnf install zoneminder-nginx -y
