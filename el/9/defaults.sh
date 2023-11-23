98f3777bd53593b9bcf7d4629142d929eb9e88d4
fishbone222 <github@fischer-siegen.de>
2023-11-23 11:44:14 +0100

    Update defaults.sh

--- el/9/defaults.sh
+++ el/9/defaults.sh
@@ -3,7 +3,7 @@ dnf install -y epel-release
 dnf install -y vim htop nmap wget openssh-server tar unzip
 timedatectl set-timezone Europe/Berlin
 systemctl enable --now sshd
-#curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/centos/8/fix-lxc-filesystem.sh | sh
+
 dnf upgrade -y
 
 dnf autoremove -y
