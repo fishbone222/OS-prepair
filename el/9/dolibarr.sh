4790781724aec95e59abb7a380858d619884a4f9
fishbone222 <github@fischer-siegen.de>
2023-12-02 12:50:54 +0100

    Update dolibarr.sh

--- el/9/dolibarr.sh
+++ el/9/dolibarr.sh
@@ -1,5 +1,7 @@
-# defaults
-# dnf-auto
-# nginx
-# mariadb
+#curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/dolibarr.sh | bash
+#LXC unpreviliged, nesting
+curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/defaults.sh | bash
+curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/dnf_auto.sh | bash
+curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/nginx.sh | bash
+curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/mariadb.sh | bash
 dnf install -y php-gd php-zip php-intl php-mysqli
