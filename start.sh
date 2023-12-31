17fb8e1212032bc09800c2bbb6e26923d0d1356c
fishbone222 <github@fischer-siegen.de>
2023-12-31 14:11:57 +0100

    Update start.sh
    compatible with almalinux

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
+git clone https://github.com/fishbone222/OS-prepair/ /tmp/osprepair
+cd /tmp/osprepair/
 source /etc/os-release
 RELEASE=$(echo $VERSION_ID | cut -d '.' -f1)
+case $ID in
+   almalinux|centos|rhel)
+     ID2="el" ;;
+   *)
+     ID2=$ID ;;
+esac
 bash $ID/$RELEASE/defaults.sh
 echo "Scripts found for $ID Version $RELEASE:"
 pwd
