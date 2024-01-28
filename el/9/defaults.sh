e9c1222022047894358b0dc7f3139859636bee1d
fishbone222 <github@fischer-siegen.de>
2024-01-28 13:31:33 +0100

    Update defaults.sh

--- el/9/defaults.sh
+++ el/9/defaults.sh
@@ -1,6 +1,8 @@
 #curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/defaults.sh | bash
 dnf install -y epel-release
-dnf install -y vim htop nmap wget openssh-server tar unzip
+dnf install -y 'dnf-command(config-manager)'
+dnf config-manager --set-enabled crb
+dnf install -y vim htop nmap wget openssh-server tar unzip bash-completion
 timedatectl set-timezone Europe/Berlin
 curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/id_rsa.pub >> ~/.ssh/authorized_keys
 systemctl enable --now sshd
