158c9dcdfd851f640ea69c1362a9520971fde6db
fishbone222 <github@fischer-siegen.de>
2024-04-23 20:09:01 +0200

    Update collabora.sh

--- el/9/collabora.sh
+++ el/9/collabora.sh
@@ -1,6 +1,6 @@
 wget https://www.collaboraoffice.com/repos/CollaboraOnline/CODE-centos7/repodata/repomd.xml.key && sudo rpm --import repomd.xml.key
 yum config-manager --add-repo https://www.collaboraoffice.com/repos/CollaboraOnline/CODE-rpm
-yum install coolwsd CODE-brand
+yum install coolwsd CODE-brand -y
 cp /tmp/*.key /etc/coolwsd/key.pem
 cp /tmp/*.fullchain /etc/coolwsd/cert.pem
 cp /tmp/*.ca /etc/coolwsd/ca-chain.cert.pem
