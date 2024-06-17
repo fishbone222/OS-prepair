a574ed642c10eb00e1b50d2bd781671be2d2c1f6
fishbone222 <github@fischer-siegen.de>
2024-06-17 21:35:01 +0200

    Update freepbx.sh

--- debian/12/freepbx.sh
+++ debian/12/freepbx.sh
@@ -1,32 +1,96 @@
 #deb12 lxc unprivileged 10gb ssd
 apt update -y
 apt upgrade -y
-apt install curl lsb-release git -y
+apt -y install lsb-release build-essential git curl wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev libjansson-dev libxml2-dev uuid-dev default-libmysqlclient-dev htop sngrep lame ffmpeg mpg123 vim expect build-essential linux-headers-`uname -r` openssh-server apache2 mariadb-server mariadb-client bison flex php8.2 php8.2-curl php8.2-cli php8.2-common php8.2-mysql php8.2-gd php8.2-mbstring  php8.2-intl php8.2-xml php-pear curl sox libncurses5-dev libssl-dev mpg123 libxml2-dev libnewt-dev sqlite3  libsqlite3-dev pkg-config automake libtool autoconf git unixodbc-dev uuid uuid-dev libasound2-dev libogg-dev libvorbis-dev libicu-dev libcurl4-openssl-dev odbc-mariadb libical-dev libneon27-dev libsrtp2-dev  libspandsp-dev sudo subversion libtool-bin python-dev-is-python3 unixodbc vim wget libjansson-dev software-properties-common nodejs npm ipset iptables fail2ban php-soap
 systemctl stop apparmor
 apt remove apparmor -y
-cd /usr/src
+
 #Asterisk 20
+cd /usr/src
 wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
-tar zxvf asterisk-20-current.tar.gz
-rm -rf asterisk-20-current.tar.gz
+tar xvf asterisk-20-current.tar.gz
 cd asterisk-20*/
 astdir=$(pwd)
 astver=${astdir##*-}
+contrib/scripts/get_mp3_source.sh
 contrib/scripts/install_prereq install
-./configure
+./configure  --libdir=/usr/lib64 --with-pjproject-bundled --with-jansson-bundled
+make menuselect.makeopts
+menuselect/menuselect --enable app_macro menuselect.makeopts
 make menuselect
 make
 make install
 make samples
-mkdir /etc/asterisk/samples
-mv /etc/asterisk/*.*  /etc/asterisk/samples/
-make basic-pbx
 make config
-systemctl enable --now asterisk.service
+ldconfig
 
-#freePBX
-wget https://github.com/FreePBX/sng_freepbx_debian_install/raw/master/sng_freepbx_debian_install.sh -O /tmp/sng_freepbx_debian_install.sh
-bash /tmp/sng_freepbx_debian_install.sh
+groupadd asterisk
+useradd -r -d /var/lib/asterisk -g asterisk asterisk
+usermod -aG audio,dialout asterisk
+chown -R asterisk:asterisk /etc/asterisk
+chown -R asterisk:asterisk /var/{lib,log,spool}/asterisk
+chown -R asterisk:asterisk /usr/lib64/asterisk
+   
+sed -i 's|#AST_USER|AST_USER|' /etc/default/asterisk
+sed -i 's|#AST_GROUP|AST_GROUP|' /etc/default/asterisk
+sed -i 's|;runuser|runuser|' /etc/asterisk/asterisk.conf
+sed -i 's|;rungroup|rungroup|' /etc/asterisk/asterisk.conf
+echo "/usr/lib64" >> /etc/ld.so.conf.d/x86_64-linux-gnu.conf
+ldconfig
+
+sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php/8.2/apache2/php.ini
+sed -i 's/\(^memory_limit = \).*/\1256M/' /etc/php/8.2/apache2/php.ini
+sed -i 's/^\(User\|Group\).*/\1 asterisk/' /etc/apache2/apache2.conf
+sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+a2enmod rewrite
+systemctl restart apache2
+rm /var/www/html/index.html
+
+cat <<EOF > /etc/odbcinst.ini
+[MySQL]
+Description = ODBC for MySQL (MariaDB)
+Driver = /usr/lib/x86_64-linux-gnu/odbc/libmaodbc.so
+FileUsage = 1
+EOF
+
+cat <<EOF > /etc/odbc.ini
+[MySQL-asteriskcdrdb]
+Description = MySQL connection to 'asteriskcdrdb' database
+Driver = MySQL
+Server = localhost
+Database = asteriskcdrdb
+Port = 3306
+Socket = /var/run/mysqld/mysqld.sock
+Option = 3
+EOF
+
+cd /usr/local/src
+wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-17.0-latest-EDGE.tgz
+tar zxvf freepbx-17.0-latest-EDGE.tgz
+cd /usr/local/src/freepbx/
+./start_asterisk start
+./install -n
+
+fwconsole ma installall
+fwconsole reload
+fwconsole restart
+
+cat <<EOF > /etc/systemd/system/freepbx.service
+[Unit]
+Description=FreePBX VoIP Server
+After=mariadb.service
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/sbin/fwconsole start -q
+ExecStop=/usr/sbin/fwconsole stop -q
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now freepbx
+
 
 #chan_dongle
 cd /tmp/
