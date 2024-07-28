4df8611fca14f476fed7593bb6f33571a2793fd1
fishbone222 <github@fischer-siegen.de>
2024-07-28 10:49:49 +0200

    Create photoprism.sh

---
+++ debian/12/photoprism.sh
@@ -0,0 +1,4 @@
+#curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/debian/12/photoprism.sh | bash
+apt install -y curl htop
+curl -sLO https://dl.photoprism.app/pkg/linux/deb/amd64.deb
+apt install -y --no-install-recommends ./amd64.deb
