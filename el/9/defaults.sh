722e2a993e6530634d9354537f01171655ace27e
fishbone222 <github@fischer-siegen.de>
2025-04-07 06:26:49 +0200

    Update defaults.sh
    moved ssh to top

--- el/9/defaults.sh
+++ el/9/defaults.sh
@@ -1,12 +1,14 @@
 #curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/el/9/defaults.sh | bash
+dnf install -y openssh-server
+curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/id_rsa.pub >> ~/.ssh/authorized_keys
+systemctl enable --now sshd
+
+echo 'alias ls="ls -hal --color=auto"'>>~/.bashrc
 dnf install -y epel-release
 dnf install -y 'dnf-command(config-manager)'
 dnf config-manager --set-enabled crb
-dnf install -y vim htop nmap wget openssh-server tar unzip bash-completion
+dnf install -y vim htop nmap wget tar unzip bash-completion
 timedatectl set-timezone Europe/Berlin
-curl https://raw.githubusercontent.com/fishbone222/OS-prepair/main/id_rsa.pub >> ~/.ssh/authorized_keys
-systemctl enable --now sshd
-echo 'alias ls="ls -hal --color=auto"'>>~/.bashrc
 
 dnf upgrade -y
 
