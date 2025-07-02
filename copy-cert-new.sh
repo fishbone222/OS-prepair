4fd9c4e30ce0e1ba6f9e5312d362342662a265a4
fishbone222 <github@fischer-siegen.de>
2025-07-02 13:59:16 +0200

    Update copy-cert-new.sh

--- copy-cert-new.sh
+++ copy-cert-new.sh
@@ -1,12 +1,13 @@
 domain=$(hostname -d)
 
-scp certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/fullchain.cer /tmp/cert.pem
+scp -4 certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/fullchain.cer /tmp/cert.pem
 diff /tmp/cert.pem /etc/pki/nginx/server.crt > /dev/null
 if [ $? -eq 0 ]; then
         echo "Cert not due to renewal"
         exit 0
 fi
-scp certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/fullchain.cer /etc/pki/nginx/server.crt
-scp certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/*$domain.cer /etc/pki/nginx/server.single.crt
-scp certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/*.key /etc/pki/nginx/private/server.key
+scp -4 certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/fullchain.cer /etc/pki/nginx/server.crt
+scp -4 certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/*$domain.cer /etc/pki/nginx/server.single.crt
+scp -4 certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/*.key /etc/pki/nginx/private/server.key
+scp -4 certificate@opnsense.$domain:/var/etc/acme-client/cert-home/*/\*.$domain/ca.cer /etc/pki/nginx/private/ca.crt
 systemctl reload nginx
